ec6cb13d10f6e0e31e9a7f43659fcf5a88f93809
杨强 <804061376@qq.com>
2020-03-07 11:13:40 +0800

    工具类优化

--- src/main/java/com/gabriel/stage/utils/LocalDataTimeUtils.java
+++ src/main/java/com/gabriel/stage/utils/LocalDataTimeUtils.java
@@ -16,27 +16,29 @@ import java.util.Date;
  */
 public class LocalDataTimeUtils {
 
-    public static final String formatter_yyyyMMddHHmmss = "yyyy-MM-dd HH:mm:ss";
-    public static final String formatter_yyyyMMddHHmm = "yyyy-MM-dd HH:mm";
-    public static final String formatter_yyyyMMdd = "yyyy-MM-dd";
+    public static final String FORMATTER_YYYY_M_MDD_H_HMMSS = "yyyy-MM-dd HH:mm:ss";
+
+    public static final String FORMATTER_YYYY_M_MDD_H_HMM = "yyyy-MM-dd HH:mm";
+
+    public static final String FORMATTER_YYYY_M_MDD = "yyyy-MM-dd";
 
     /**
-    * @Description: 将Date类型转化为指定格式字符串
-    * @Param: [date, format]
-    * @return: java.lang.String
-    * @Date: 2019-11-26
-    */
+     * 将Date类型转化为指定格式string
+     * @param date
+     * @param format
+     * @return
+     */
     public static String getStringByDate(Date date, String format) {
         SimpleDateFormat sdf = new SimpleDateFormat(format);
         return sdf.format(date);
     }
 
     /**
-    * @Description: 将指定格式的字符串转化为Date
-    * @Param: [dateStr, format]
-    * @return: java.util.Date
-    * @Date: 2019-11-26
-    */
+     * 将String类型转化为指定格式Date类型
+     * @param dateStr
+     * @param format
+     * @return
+     */
     public static Date getDateByString(String dateStr, String format) {
         SimpleDateFormat sdf = new SimpleDateFormat(format);
         try {
@@ -48,30 +50,27 @@ public class LocalDataTimeUtils {
     }
 
     /**
-    * @Description: 获取当前Date并转化为指定格式字符串
-    * @Param: [format]
-    * @return: java.lang.String
-    * @Date: 2019-11-26
-    */
+     * 获取当前Date并转化为指定格式字符串
+     * @param format
+     * @return
+     */
     public static String getCurrentDate(String format) {
         return getStringByDate(new Date(), format);
     }
 
     /**
-     * @Description: Date转换为LocalDateTime
-     * @Param: [date]
-     * @return: java.time.LocalDateTime
-     * @Date: 2019-11-26
+     * Date转换为LocalDateTime
+     * @param date
+     * @return
      */
     public static LocalDateTime convertDateToLDT(Date date) {
         return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
     }
 
     /**
-     * @Description: LocalDateTime转换为Date
-     * @Param: [time]
-     * @return: java.util.Date
-     * @Date: 2019-11-26
+     * LocalDateTime转换为Date
+     * @param time
+     * @return
      */
     public static Date convertLDTToDate(LocalDateTime time) {
         return Date.from(time.atZone(ZoneId.systemDefault()).toInstant());
@@ -79,60 +78,59 @@ public class LocalDataTimeUtils {
 
 
     /**
-     * @Description: 获取指定日期的秒
-     * @Param: [time]
-     * @return: java.lang.Long
-     * @Date: 2019-11-26
+     * 获取指定日期的秒
+     * @param time
+     * @return
      */
     public static Long getMilliByTime(LocalDateTime time) {
         return time.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
     }
 
     /**
-     * @Description: 获取指定日期的秒
-     * @Param: [time]
-     * @return: java.lang.Long
-     * @Date: 2019-11-26
+     * 获取指定日期的秒
+     * @param time
+     * @return
      */
     public static Long getSecondsByTime(LocalDateTime time) {
         return time.atZone(ZoneId.systemDefault()).toInstant().getEpochSecond();
     }
 
     /**
-     * @Description: 获取指定时间的指定格式
-     * @Param: [time, pattern]
-     * @return: java.lang.String
-     * @Date: 2019-11-26
+     * 获取指定时间的指定格式
+     * @param time
+     * @param pattern
+     * @return
      */
     public static String formatTime(LocalDateTime time,String pattern) {
         return time.format(DateTimeFormatter.ofPattern(pattern));
     }
 
     /**
-     * @Description: 获取当前时间的指定格式
-     * @Param: [pattern]
-     * @return: java.lang.String
-     * @Date: 2019-11-26
+     * 获取当前时间的指定格式
+     * @param pattern
+     * @return
      */
     public static String formatNow(String pattern) {
         return  formatTime(LocalDateTime.now(), pattern);
     }
 
     /**
-     * @Description: 日期加上一个数,根据field不同加不同值,field为ChronoUnit.*
-     * @Param: [time, number, field]
-     * @return: java.time.LocalDateTime
-     * @Date: 2019-11-26
+     * 日期加上一个数,根据field不同加不同值,field为ChronoUnit.*
+     * @param time
+     * @param number
+     * @param field
+     * @return
      */
     public static LocalDateTime plus(LocalDateTime time, long number, TemporalUnit field) {
         return time.plus(number, field);
     }
 
     /**
-     * @Description: 日期减去一个数,根据field不同减不同值,field参数为ChronoUnit.*
-     * @Param: [time, number, field]
-     * @return: java.time.LocalDateTime
-     * @Date: 2019-11-26
+     * 日期减去一个数,根据field不同减不同值,field参数为ChronoUnit.*
+     * @param time
+     * @param number
+     * @param field
+     * @return
      */
     public static LocalDateTime minu(LocalDateTime time, long number, TemporalUnit field){
         return time.minus(number,field);
@@ -142,7 +140,7 @@ public class LocalDataTimeUtils {
      * 获取两个日期的差  field参数为ChronoUnit.*
      * @param startTime
      * @param endTime
-     * @param field  单位(年月日时分秒)
+     * @param field
      * @return
      */
     public static long betweenTwoTime(LocalDateTime startTime, LocalDateTime endTime, ChronoUnit field) {
@@ -153,11 +151,11 @@ public class LocalDataTimeUtils {
     }
 
     /**
-    * @Description: 比较两个Date的时间大小
-    * @Param: [date, date2]
-    * @return: boolean
-    * @Date: 2019-11-26
-    */
+     * 比较两个Date的时间大小
+     * @param date
+     * @param date2
+     * @return
+     */
     public static boolean getLocalDateTime(Date date, Date date2) {
         if (date.compareTo(date2) == 1) {
             return true;
@@ -166,122 +164,111 @@ public class LocalDataTimeUtils {
     }
 
     /**
-    * @Description: 将LocalDateTime转化为Date
-    * @Param: [localDateTime]
-    * @return: java.util.Date
-    * @Date: 2019-11-26
-    */
+     * 将LocalDateTime转化为Date
+     * @param localDateTime
+     * @return
+     */
     public static Date getDataByLocalDateTime(LocalDateTime localDateTime) {
         return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
     }
 
     /**
-    * @Description: 将指定格式String类型日期转化为LocalDateTime
-    * @Param: [format, localDateTime]
-    * @return: java.time.LocalDateTime
-    * @Date: 2019-11-26
-    */
+     * 将指定格式String类型日期转化为LocalDateTime
+     * @param format
+     * @param localDateTime
+     * @return
+     */
     public static LocalDateTime getLocalDateTimeByString(String format,String localDateTime) {
         DateTimeFormatter df= DateTimeFormatter.ofPattern(format);
         return LocalDateTime.parse(localDateTime, df);
     }
 
     /**
-    * @Description: 获取指定LocalDateTime的秒数
-    * @Param: [localDateTime]
-    * @return: java.lang.Long
-    * @Date: 2019-11-26
-    */
+     * 获取指定LocalDateTime的秒数
+     * @param localDateTime
+     * @return
+     */
     public static Long getSecondByLocalDateTime(LocalDateTime localDateTime) {
         return localDateTime.atZone(ZoneId.systemDefault()).toInstant().getEpochSecond();
     }
 
     /**
-    * @Description: 获取指定LocalDateTime的毫秒数
-    * @Param: [localDateTime]
-    * @return: java.lang.Long
-    * @Date: 2019-11-26
-    */
+     * 获取指定LocalDateTime的毫秒数
+     * @param localDateTime
+     * @return
+     */
     public static Long getMillSecondByLocalDateTime(LocalDateTime localDateTime) {
         return localDateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
     }
 
     /**
-    * @Description: 将LocalDateTime转化为指定格式字符串
-    * @Param: [localDateTime, format]
-    * @return: java.lang.String
-    * @Date: 2019-11-26
-    */
+     * 将LocalDateTime转化为指定格式字符串
+     * @param localDateTime
+     * @param format
+     * @return
+     */
     public static String getStringByLocalDateTime(LocalDateTime localDateTime, String format) {
         return localDateTime.format(DateTimeFormatter.ofPattern(format));
     }
 
     /**
-    * @Description: 将LocalDateTime转化为默认yy-mm-dd hh:mm:ss格式字符串
-    * @Param: [localDateTime]
-    * @return: java.lang.String
-    * @Date: 2019-11-26
-    */
+     * 将LocalDateTime转化为默认yy-mm-dd hh:mm:ss格式字符串
+     * @param localDateTime
+     * @return
+     */
     public static String getDefaultStringByLocalDateTime(LocalDateTime localDateTime) {
-        return getStringByLocalDateTime(localDateTime, formatter_yyyyMMddHHmmss);
+        return getStringByLocalDateTime(localDateTime, FORMATTER_YYYY_M_MDD_H_HMMSS);
     }
 
     /**
-    * @Description: 获取当前LocalDateTime并转化为指定格式字符串
-    * @Param: [format]
-    * @return: java.lang.String
-    * @Date: 2019-11-26
-    */
+     * 获取当前LocalDateTime并转化为指定格式字符串
+     * @param format
+     * @return
+     */
     public static String getCurrentLocalDateTime(String format) {
         return getStringByLocalDateTime(LocalDateTime.now(), format);
     }
 
     /**
-    * @Description: 获取一天的开始时间
-    * @Param: [localDateTime]
-    * @return: java.lang.String
-    * @Date: 2019-11-26
-    */
+     * 获取某天的开始时间
+     * @param localDateTime
+     * @return
+     */
     public static String getStartTime(LocalDateTime localDateTime) {
         return getDefaultStringByLocalDateTime(localDateTime.with(LocalTime.MIN));
     }
 
     /**
-    * @Description: 获取当天的开始时间
-    * @Param: []
-    * @return: java.lang.String
-    * @Date: 2019-11-26
-    */
+     * 获取一天的开始时间
+     * @return
+     */
     public static String getStartTime() {
         return getDefaultStringByLocalDateTime(LocalDateTime.now().with(LocalTime.MIN));
     }
 
     /**
-    * @Description: 获取一天的结束时间
-    * @Param: [localDateTime]
-    * @return: java.lang.String
-    * @Date: 2019-11-26
-    */
+     * 获取一天的结束时间
+     * @param localDateTime
+     * @return
+     */
     public static String getEndTime(LocalDateTime localDateTime) {
         return getDefaultStringByLocalDateTime(localDateTime.with(LocalTime.MAX));
     }
 
     /**
-    * @Description: 获取当天的结束时间
-    * @Param: []
-    * @return: java.lang.String
-    * @Date: 2019-11-26
-    */
+     * 获取当天的结束时间
+     * @return
+     */
     public static String getEndTime() {
         return getDefaultStringByLocalDateTime(LocalDateTime.now().with(LocalTime.MAX));
     }
 
     /**
-    * @Description: 比较两个LocalDateTime的时间大小
-    * @Param: [localDateTime1, localDateTime2]
-    * @return: boolean
-    * @Date: 2019-11-26
-    */
+     * 比较两个LocalDateTime的时间大小
+     * @param localDateTime1
+     * @param localDateTime2
+     * @return
+     */
     public static boolean getCompareLocalDateTime(LocalDateTime localDateTime1, LocalDateTime localDateTime2) {
         if (localDateTime1.isBefore(localDateTime2)) {
             return true;
@@ -290,11 +277,12 @@ public class LocalDataTimeUtils {
     }
 
     /**
-    * @Description: 计算两个指定格式String日期字符串的时间差(精确到毫秒)
-    * @Param: [format, time1, time2]
-    * @return: java.lang.Long
-    * @Date: 2019-11-26
-    */
+     * 计算两个指定格式String日期字符串的时间差(精确到毫秒)
+     * @param format
+     * @param time1
+     * @param time2
+     * @return
+     */
     public static Long getCompareSecondLocalDateTime(String format ,String time1, String time2) {
         System.out.println("接收到的参数为:"+format+","+time1+","+time2);
         LocalDateTime localDateTime1=getLocalDateTimeByString(format,time1);
@@ -306,11 +294,11 @@ public class LocalDataTimeUtils {
     }
 
     /**
-    * @Description: 获取两个LocalDateTime的天数差
-    * @Param: [localDateTime1, localDateTime2]
-    * @return: java.lang.Long
-    * @Date: 2019-11-26
-    */
+     * 获取两个LocalDateTime的天数差
+     * @param localDateTime1
+     * @param localDateTime2
+     * @return
+     */
     public static Long getCompareDayLocalDateTime(LocalDateTime localDateTime1, LocalDateTime localDateTime2) {
         if (getCompareLocalDateTime(localDateTime1, localDateTime2)) {
             Duration duration = Duration.between(localDateTime1, localDateTime2);
@@ -322,11 +310,11 @@ public class LocalDataTimeUtils {
     }
 
     /**
-    * @Description: 获取两个LocalDateTime的小时差
-    * @Param: [localDateTime1, localDateTime2]
-    * @return: java.lang.Long
-    * @Date: 2019-11-26
-    */
+     * 获取两个LocalDateTime的小时差
+     * @param localDateTime1
+     * @param localDateTime2
+     * @return
+     */
     public static Long getCompareYearLocalDateTime(LocalDateTime localDateTime1, LocalDateTime localDateTime2) {
         if (getCompareLocalDateTime(localDateTime1, localDateTime2)) {
             Duration duration = Duration.between(localDateTime1, localDateTime2);
